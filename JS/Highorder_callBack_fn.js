a984f32e96886d094abc27c29cfa5fdb718ae76f
ashish03440 <ashishkumar03440@gmail.com>
2025-06-29 22:06:42 +0530

    theory update

--- JS/Highorder_callBack_fn.js
+++ JS/Highorder_callBack_fn.js
@@ -12,14 +12,19 @@
 
 
 // ##################### callback function #################
-function fun1() {
-    function sayhi() {
-        console.log("hii")
-    }
-    return sayhi;
-}
+// function fun1() {
+//   function sayhi() {
+//     console.log("hii") // agar hum log return pass nahi karinage toh yai hii , undefined print karega 
+//     return "hii" ;// iske baad yai hii , hii return karega 
+//   }
+//   return sayhi;// agar "sayhi" hai toh poora "sayhi" functioon return hoga agar sayhi()  hai toh hii return hoga 
+// }
 
-let ans = fun1()
+// let ans = fun1()// agar hum fun1 main sayhi return kar rahe hai toh yahan par sayhi() wala poora function aayega 
+//                 // agar hum fun1 main sayhi() retun kar rahe hai toh ans ke anar undefined pass hoga 
 
-// console.log(ans)
-ans()
+// // console.log(ans)
+// ans();// agar ismain sayhi() aayega toh yai hii print kargea 
+//       // agar ismain unndefind aayega toh yai not a function print karega 
+
+
