cf1c4c93943aa7b8bdcef441928281a174a2f4b3
ashish03440 <ashishkumar03440@gmail.com>
2025-06-30 22:06:52 +0530

    timeout

---
+++ JS/timeout.js
@@ -0,0 +1,9 @@
+document.writeln("ashish");
+
+let a = setTimeout(function() {
+    alert("i am inside the timr intervel ")
+    document.writeln("ashish");
+
+}, 5000);
+// clearTimeout(a);
+console.log(a)
